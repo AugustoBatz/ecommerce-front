660690b4e5473f12d8c3564f0b467c2d41a21307
Cristian1Alexar <cristian.archilac@gmail.com>
2020-09-09 21:20:35 -0600

    se creó el componente para registro de usuario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/general/navbar/navbar.component';
 import { HeaderComponent } from './components/general/header/header.component';
 import { SignInComponent } from './components/user/sign-in/sign-in.component';
+import { SignUpComponent } from './components/user/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeaderComponent,
-    SignInComponent
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
