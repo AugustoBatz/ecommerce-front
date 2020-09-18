dea407d89be73e1989fc7411eeb43676be87245b
Cristian1Alexar <cristian.archilac@gmail.com>
2020-09-18 00:53:54 -0600

    mediante angular forms se añadió el manejador de formulario para captar los datos ingresados por el usuario

--- src/app/components/user/sign-in/sign-in.component.ts
+++ src/app/components/user/sign-in/sign-in.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Login} from '../../../models/login_request'
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.sass']
 })
 export class SignInComponent implements OnInit {
+  signInForm: FormGroup; 
+  login: Login
 
-  constructor() { }
+  constructor(private initForm: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signInForm = this.initForm.group({
+      email: ['', Validators.compose([Validators.email, Validators.required])],
+      password: ['', Validators.required]
+    })
+
+    this.signInForm.valueChanges.subscribe(()=>{this.login = this.signInForm.value; console.log(this.login)})
   }
 
 }
