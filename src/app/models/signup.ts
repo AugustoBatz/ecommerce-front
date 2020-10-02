3b63f66a80c698b73098903acb591287e9b748fd
Cristian1Alexar <cristian.archilac@gmail.com>
2020-10-01 19:17:23 -0600

    modelo para el request del signup de usuario

---
+++ src/app/models/signup.ts
@@ -0,0 +1,10 @@
+export interface Signup{
+    first_name: string,
+    last_name: string,
+    phone: string,
+    address_a: string,
+    address_b: string,
+    email: string,
+    password: string,
+    username: string
+}
