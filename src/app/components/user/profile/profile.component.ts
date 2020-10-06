429cb31240ddfa696d95fd48a90f189104473e23
Cristian1Alexar <cristian.archilac@gmail.com>
2020-10-05 21:50:31 -0600

    Se modificó el componente de perfil para que realice una petición a la API y despliegue los datos del usuario actual

--- src/app/components/user/profile/profile.component.ts
+++ src/app/components/user/profile/profile.component.ts
@@ -3,6 +3,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Profile } from 'src/app/models/profile_data';
 //import { DataService } from "src/app/services/data/data.service";
 import {  UsersService} from "src/app/components/user/profile/user.service";
+import { APIService } from 'src/app/services/backend/api.service';
+import { User} from '../../../models/user';
+import { DataService } from 'src/app/services/data/data.service';
 
 
 @Component({
@@ -13,13 +16,26 @@ import {  UsersService} from "src/app/components/user/profile/user.service";
 export class ProfileComponent implements OnInit {
   profile: Profile;
   users: any;
-  constructor( public usersService: UsersService ) { }
+  constructor( public usersService: UsersService, private apiService: APIService, public data: DataService ) { }
 
   ngOnInit() {
     this.usersService.getUsers().subscribe(data => {
       this.users = data;
       console.log(data);
     });
+    this.getUser();
   } 
+  getUser(){
+    if (this.data.current_user.username == ""){
+      this.apiService.userData().subscribe(
+        (res: User) => {
+          this.data.set_user(res); 
+          console.log(this.data.user);
+        }
+          , 
+        err => console.log(err)
+      ); 
+    }
+  }
     
 }
