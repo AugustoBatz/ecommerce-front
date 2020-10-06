960bd3c83a349b0b0b663fd75c25fdc48a383258
Cristian1Alexar <cristian.archilac@gmail.com>
2020-10-05 21:55:12 -0600

    se modificó el servicio de data para poder manejar la información de usuario entre componentes

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { User } from 'src/app/models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-
+  current_user: User = {
+    first_name: "",
+    username: "",
+    last_name: "",
+    phone: "",
+    address_a: "",
+    address_b: "",
+    email: "",
+    is_admin: true
+};
   constructor() { }
+  resetUser(){
+    this.current_user = {
+      first_name: "",
+      username: "",
+      last_name: "",
+      phone: "",
+      address_a: "",
+      address_b: "",
+      email: "",
+      is_admin: true
+  };
+  }
+  public get user(): User{
+    return this.current_user;
+  }
+  public set_user(user: User){
+    this.current_user = user;
+  }
 }
