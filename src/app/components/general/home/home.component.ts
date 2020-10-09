a4cc727e79505d64cb3b5e36a56cc5450e2162d8
Cristian1Alexar <cristian.archilac@gmail.com>
2020-10-08 20:46:06 -0600

    Se instaló dependencia llamada lozad para lazy loading de imagenes

--- src/app/components/general/home/home.component.ts
+++ src/app/components/general/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import lozad from 'lozad';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,8 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+    observer.observe();
   }
 
 }
