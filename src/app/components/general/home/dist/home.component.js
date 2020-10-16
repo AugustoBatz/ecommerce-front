98a763ce8590862e7ddb35ded483134bee3a0a2f
Cristian1Alexar <cristian.archilac@gmail.com>
2020-10-15 20:58:00 -0600

    cambios en el estilo de la página de inicio

---
+++ src/app/components/general/home/dist/home.component.js
@@ -0,0 +1,28 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+exports.HomeComponent = void 0;
+var core_1 = require("@angular/core");
+var lozad_1 = require("lozad");
+var HomeComponent = /** @class */ (function () {
+    function HomeComponent() {
+    }
+    HomeComponent.prototype.ngOnInit = function () {
+        var observer = lozad_1["default"](); // lazy loads elements with default selector as '.lozad'
+        observer.observe();
+    };
+    HomeComponent = __decorate([
+        core_1.Component({
+            selector: 'app-home',
+            templateUrl: './home.component.html',
+            styleUrls: ['./home.component.sass']
+        })
+    ], HomeComponent);
+    return HomeComponent;
+}());
+exports.HomeComponent = HomeComponent;
