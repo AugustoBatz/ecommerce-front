9ff4e5e6d7508e7ed094c1e2050297438cc615e2
Cristian1Alexar <cristian.archilac@gmail.com>
2020-11-09 00:44:18 -0600

    funcionalidad para mostrar catalogo

--- src/app/components/store/list-products/list-products.component.ts
+++ src/app/components/store/list-products/list-products.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { APIService } from 'src/app/services/backend/api.service';
+import { DataService } from 'src/app/services/data/data.service';
+import { Catalog } from '../../../models/catalog_item';
 
 @Component({
   selector: 'app-list-products',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiService: APIService, private dataService: DataService, private router: Router) { }
+  public catalog: Catalog[];
   ngOnInit(): void {
+    this.fill_catalog();
   }
-
+  fill_catalog(){
+    this.apiService.getProduct_List()
+    .subscribe(
+      (res:Catalog[]) => {
+        this.catalog = res;
+        console.log(this.catalog);
+      },
+      err=>console.log(err)
+    )
+  }
+  set_product(code: string){
+    localStorage.setItem('code', code);
+    this.router.navigate(['store/product']);
+  }
 }
