12b5d5eb1fb368c6e8a68aa48bf39ee8ce102b4d
Cristian1Alexar <cristian.archilac@gmail.com>
2020-11-10 02:33:59 -0600

    se agregó el manejo de contenido de la API

--- src/app/components/general/contact-us/contact-us.component.ts
+++ src/app/components/general/contact-us/contact-us.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Content } from 'src/app/models/content';
+import { APIService } from 'src/app/services/backend/api.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private apiService: APIService) { }
+  public content: Content = {
+    name: '',
+    pr_1: '', 
+    pr_2: '', 
+    pr_3: '', 
+    img_1: '', 
+    img_2: '', 
+    img_3: ''
+  };
   ngOnInit(): void {
+    this.getContent();
   }
-
+  getContent(){
+    this.apiService.getContentData('3').subscribe(
+      (res: Content) => {
+        this.content = res;
+      }, 
+      err => console.log(err)
+    );
+  }
 }
