c3ee2c93515f64f2eea899660a380acee8918f9a
Cristian1Alexar <cristian.archilac@gmail.com>
2020-11-12 01:15:25 -0600

    se agregó la funcionalidad para mostrar cuántos productos hay en el carrito

--- src/app/components/general/navbar/navbar.component.ts
+++ src/app/components/general/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router, RouterModule} from '@angular/router';
 import { APIService } from 'src/app/services/backend/api.service';
+import { DataService } from 'src/app/services/data/data.service';
 @Component({
   selector: 'navbar',
   templateUrl: './navbar.component.html',
@@ -8,13 +9,15 @@ import { APIService } from 'src/app/services/backend/api.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router: Router, private api: APIService) { }
+  constructor(private router: Router, private api: APIService, private dataService: DataService) { }
   login = true;
   ngOnInit(): void {
   }
   public cambiar(){
     this.login = !this.login;
-    console.log(this.login);
+  }
+  getItems(){
+    return parseInt(localStorage.getItem('items'));
   }
   logged(){
     return this.api.logIn;
