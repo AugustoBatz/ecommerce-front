2213b6a7300b49066f6b585e451d77526afeb3b6
Cristian1Alexar <cristianzxc1626@gmail.com>
2020-11-13 01:31:13 -0600

    Se agregó la función para registrar la solicitud del usuario de contacto

--- src/app/components/general/contact-us/contact-us.component.ts
+++ src/app/components/general/contact-us/contact-us.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Content } from 'src/app/models/content';
 import { APIService } from 'src/app/services/backend/api.service';
+import { Contact } from 'src/app/models/contact';
 
 @Component({
   selector: 'app-contact-us',
@@ -19,6 +20,12 @@ export class ContactUsComponent implements OnInit {
     img_2: '', 
     img_3: ''
   };
+  public contactForm: Contact = {
+    name: '',
+    useremail: '',
+    asunt: '', 
+    message: ''
+  }
   ngOnInit(): void {
     this.getContent();
   }
@@ -28,6 +35,21 @@ export class ContactUsComponent implements OnInit {
         this.content = res;
       }, 
       err => console.log(err)
+    );
+  }
+  insertContact(){
+    this.apiService.postContact(this.contactForm)
+    .subscribe(
+      res=>{
+        alert("Hemos recibido tu mensaje");
+        this.contactForm = {
+          name: '',
+          useremail: '',
+          asunt: '', 
+          message: ''
+        }
+      },
+      err=>console.log(err)
     );
   }
 }
