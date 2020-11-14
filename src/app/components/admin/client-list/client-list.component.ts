655d8663d5c918e67ff957fc7d91327789ccbaf9
Cristian1Alexar <cristianzxc1626@gmail.com>
2020-11-14 00:24:48 -0600

    Se agregó la funcionalidad de recibir lista de clientes

--- src/app/components/admin/client-list/client-list.component.ts
+++ src/app/components/admin/client-list/client-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Admin } from 'src/app/models/admin_user';
+import { AdminserviceService } from 'src/app/services/admin/adminservice.service';
 
 @Component({
   selector: 'app-client-list',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-list.component.sass']
 })
 export class ClientListComponent implements OnInit {
-
-  constructor() { }
+  public client_list: Admin[] = [];
+  constructor(private adminService: AdminserviceService, private router: Router) { }
 
   ngOnInit(): void {
+    this.adminService.getClientList()
+    .subscribe(
+      (res: Admin[])=>{
+        this.client_list= res; 
+        console.log(this.client_list);
+      },
+      err=>console.log(err)
+    );
+    if(!this.adminService.logIn){
+      this.router.navigate(['admin/sign-in']);
+    }
   }
+  
 
 }
