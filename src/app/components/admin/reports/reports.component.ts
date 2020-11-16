0d7c85e6c77db413fc8bae6a81bf50a5d503974f
Cristian1Alexar <cristianzxc1626@gmail.com>
2020-11-16 05:44:38 -0600

    Funcionalidad y estructura reporte de ventas

--- src/app/components/admin/reports/reports.component.ts
+++ src/app/components/admin/reports/reports.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DateReport } from 'src/app/models/dateReport';
+import { AdminserviceService } from 'src/app/services/admin/adminservice.service';
 
 @Component({
   selector: 'app-reports',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports.component.sass']
 })
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
-
+  date_start: string = '2020-11-10'; 
+  date_finish: string = '2020-11-16'; 
+  reports: DateReport[] = []; 
+  constructor(private adminService: AdminserviceService) { }
+  
   ngOnInit(): void {
+    
+  }
+  datePickedStart(){
+    console.log(this.date_start); 
+  }
+  datePickedFinish(){
+    console.log(this.date_finish);
+  }
+  generateReport(){
+    this.adminService.generateReportDate(this.date_start, this.date_finish)
+    .subscribe(
+      (res: DateReport[])=>
+      {
+        this.reports = res;
+      },
+      err=>console.log(err)
+    );
+  }
+  reformatDate(date: string){
+    let reformat = new Date(date).toString();
+    return reformat; 
   }
 
 }
